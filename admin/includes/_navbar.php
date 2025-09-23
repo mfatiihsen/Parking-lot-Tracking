f90a85b6ace28da6d6415902ccdc1d4cf4c5a531
Muhammed Fatih ŞEN <76725866+mfatiihsen@users.noreply.github.com>
2025-09-24 01:26:26 +0300

    Start

---
+++ admin/includes/_navbar.php
@@ -0,0 +1,14 @@
+<header class="navbar">
+    <div class="navbar-logo">
+        <i class="fas fa-car-space"></i>
+        <span style="font-weight: normal;color:white">PARK<b class="highlight">HUB</b></span>
+    </div>
+    <div class="navbar-profile">
+        <i class="fa fa-bell"></i>
+        <div class="user-info">
+            <span style="color:white">Admin User</span>
+            <small>Yönetici</small>
+        </div>
+        <img src="" alt="User Avatar" class="user-avatar">
+    </div>
+</header>
---
+++ admin/index.php
@@ -0,0 +1,9 @@
+
+
+<?php
+
+header("Location:views/loginpage/login.php");
+
+exit();
+
+?>
